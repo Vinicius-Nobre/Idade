92b27cec527c02a60ce17da7245349bf0f066cf5
TI <TI@DESKTOP-5S6PD6V>
2022-02-09 12:18:47 -0300

    Adicionar arquivos de projeto.

---
+++ Idade/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade");
+
+            int idade = int.Parse(Console.ReadLine());
+
+
+            if (idade >= 0 && idade <= 11)
+            {
+                Console.WriteLine("Você é uma criança");
+            }
+            else if (idade >= 12 && idade <= 18)
+            {
+                Console.WriteLine("Você é adolescente");
+            }
+            else if (idade >= 19 && idade <= 60)
+            {
+                Console.WriteLine("Você é adulto");
+            }else
+
+            {
+                Console.WriteLine("Você é idoso");
+            }
+
+        }
+    }
+}
